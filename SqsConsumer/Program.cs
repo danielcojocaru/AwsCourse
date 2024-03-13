f9cb53bb7e50bed9f7d6e9f47e0d27cf69e2e5d1
danielcojocaru <daniel.cojocaru.ing@gmail.com>
2024-03-13 15:20:03 +0200

    add sqs publisher, consumer and api

---
+++ SqsConsumer/Program.cs
@@ -0,0 +1,31 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+CancellationTokenSource cts = new();
+AmazonSQSClient sqsClient = new();
+GetQueueUrlResponse queueUrlResponse = await sqsClient.GetQueueUrlAsync("sqsCustomers");
+ReceiveMessageRequest receiveMessagesRequest = new()
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MaxNumberOfMessages = 10,
+    AttributeNames = ["All"],
+    MessageAttributeNames = ["All"],
+};
+
+while (!cts.IsCancellationRequested)
+{
+    ReceiveMessageResponse response = await sqsClient
+        .ReceiveMessageAsync(receiveMessagesRequest, cts.Token);
+
+    Console.WriteLine($"Received {response.Messages.Count} messages.");
+
+    foreach (Message message in response.Messages)
+    {
+        Console.WriteLine(message.MessageId);
+        Console.WriteLine(message.Body);
+
+        await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle);
+    }
+
+    await Task.Delay(1000);
+}
---
+++ SqsPublisher/Program.cs
@@ -0,0 +1,34 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using Common;
+using System.Text.Json;
+
+CustomerCreated customerCreated = new()
+{
+    Id = Guid.NewGuid(),
+    Email = "daniel.cojocaru.ing@gmail.com",
+    FullName = "Daniel Cojocaru",
+    DateOfBirth = new DateTime(1988, 1, 1),
+    GitHubUsername = "danielcojocaru",
+};
+
+AmazonSQSClient sqsClient = new();
+GetQueueUrlResponse queueUrlResponse = await sqsClient.GetQueueUrlAsync("sqsCustomers");
+SendMessageRequest sendMessageRequest = new()
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customerCreated),
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>()
+    {
+        {
+            "MessageType", new MessageAttributeValue()
+            {
+                DataType = "String",
+                StringValue = nameof(CustomerCreated),
+            }
+        }
+    }
+};
+SendMessageResponse response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine();
